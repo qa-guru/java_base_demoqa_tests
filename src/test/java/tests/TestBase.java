5867c09a4bc7c267a138abb32d5a5cfd03a8774e
svasenkov <aanher@gmail.com>
2026-02-11 18:18:18 +0100

    commented browser version

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -10,7 +10,7 @@ public class TestBase {
     @BeforeAll
     static void beforeAll() {
         Configuration.browser = "chrome";
-        Configuration.browserVersion = "144.0";
+//        Configuration.browserVersion = "144.0";
         Configuration.browserSize = "1920x1080";
         Configuration.baseUrl = "https://demoqa.com";
 //        Configuration.pageLoadStrategy = "eager";
