7aa422e9cef34da184f064af9111d1f22e64f054
svasenkov <aanher@gmail.com>
2026-02-20 18:44:48 +0100

    added faker example

--- src/test/java/tests/TextBoxTests.java
+++ src/test/java/tests/TextBoxTests.java
@@ -1,6 +1,9 @@
 package tests;
 
+import com.github.javafaker.Faker;
 import org.junit.jupiter.api.Test;
+
+import java.util.Locale;
 
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.$;
@@ -44,6 +47,32 @@ public class TextBoxTests extends TestBase {
                 .checkField("name", userName)
                 .checkField("email", userEmail);
     }
+
+    @Test
+    void successfulFillFormTest_with_faker() {
+        Faker faker = new Faker();
+        Faker fakerRu = new Faker(new Locale("ru"));
+        String userName = fakerRu.name().fullName();
+        String userEmail = faker.internet().emailAddress();
+        String currentAddress = fakerRu.address().fullAddress();
+        String permanentAddress = fakerRu.address().fullAddress();
+
+        textBoxPage.openPage();
+        textBoxPage.typeUserName(userName);
+        textBoxPage.typeUserEmail(userEmail);
+        $("[id=currentAddress]").setValue(currentAddress);
+        $("[id=permanentAddress]").setValue(permanentAddress);
+        textBoxPage.submitForm();
+
+        textBoxPage.checkField("name", userName);
+        textBoxPage.checkField("email", userEmail);
+        $("[id=output] [id=currentAddress]").shouldHave(text(currentAddress));
+        $("[id=output] [id=permanentAddress]").shouldHave(text(permanentAddress));
+    }
+
+
+
+
 
     @Test
     void successfulFillFormWithoutAddressTest_old() {
